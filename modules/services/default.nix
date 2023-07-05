932cd522d69aad5b57c2c302b4615b767dc5b5c7
Lactore <lactore@protonmail.com>
2023-07-05 17:50:14 +0200

    add services

---
+++ modules/services/default.nix
@@ -0,0 +1,16 @@
+#
+#  Services
+#
+#  flake.nix
+#   ├─ ./hosts
+#   │   └─ home.nix
+#   └─ ./modules
+#       └─ ./services
+#           └─ default.nix *
+#               └─ ...
+#
+
+[
+  ./dunst.nix
+  ./udiskie.nix
+]
