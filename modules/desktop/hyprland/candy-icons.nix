c34437a495f69459be7c36b8aeb1a9b26b8bd430
Lactore <lactore@protonmail.com>
2023-07-06 14:52:15 +0200

    update

--- modules/desktop/hyprland/candy-icons.nix
+++ modules/desktop/hyprland/candy-icons.nix
@@ -4,7 +4,7 @@
 stdenv.mkDerivation {
   name = "candy-icons";
   version = "1.0.0";
-  src = pkgs.fetchurl {
+  src = pkgs.fetchzip {
     url = "https://github.com/EliverLara/candy-icons/archive/master.zip";
     sha256 = "ee627aa567e112fc5495757143eb89d310ce1885ebe13d83c2508d6be7393f4f";
   };
